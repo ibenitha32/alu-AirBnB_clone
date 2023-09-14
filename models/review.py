5c864c0c26fe6e7030953cd3c1c6451b550a300c
ibenitha32 <b.iradukund1@alustudent.com>
2023-09-14 09:22:43 -0700

    the rest of models/

---
+++ models/review.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines the Review class, which inherits from BaseModel"""
+
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    """Review class for storing reviews"""
+
+        place_id = ""
+        user_id = ""
+        text = ""
---
+++ models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" This module defines the State class, which inherits from BaseModel. """
+
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """This class represents a State object."""
+    name = ""
